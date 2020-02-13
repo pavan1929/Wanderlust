0e92e2e0ee390b7114a667cd4bf8123aca828eb3
root <root@ip-10-0-4-217.ap-southeast-1.compute.internal>
2020-02-13 13:28:51 +0000

    register module

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup,Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm : FormGroup;
+  message : string;
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit() {
+    window.scrollTo(0, 0)
+    this.registerForm = this.formbuilder.group({
+      username : ['',Validators.required],
+      email : ['', emailMatch,Validators.required],
+      contactNo : ['',Validators.required,Validators.pattern('^[0-9]{10}$')],
+      password : ['',Validators.required]
+    })
   }
 
+}
+function register(){
+
+
+}
+function emailMatch(mail:FormControl){
+  console.log("went");
+  let regExp = /^([a-zA-Z\d-\_\.]+)@([a-zA-Z\d_\-\.]+)\.([\a\A]{2,5})$/;
+  return regExp.test(mail.value) ? null : {
+    Emailerror:{message:"Invalid Email Format"}
+  }
 }
